35f186a976d1d73437c06c4761efc1e2998aa058
Jennifer Owens <jmo.jennifer@gmail.com>
2016-12-14 10:46:17 -0800

    WIP: working on function for scoring game

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -1,39 +1,59 @@
 import Game from 'game';
 
 describe('Game', function() {
-    var testGame;
-    beforeEach(function() {
-        testGame = new Game();
-    });
+  var testGame;
+  beforeEach(function() {
+    testGame = new Game();
+  });
 
-    it('will create a board', function() {
-        expect(testGame.gameBoard).toBeDefined(true);
-    });
+  it('will create a board', function() {
+    expect(testGame.gameBoard).toBeDefined(true);
+  });
 
-    it('will create two players', function() {
-        expect(testGame.playerX).toBeDefined(true);
-        expect(testGame.playerO).toBeDefined(true);
-    });
+  it('will create two players', function() {
+    expect(testGame.playerX).toBeDefined(true);
+    expect(testGame.playerO).toBeDefined(true);
+  });
 
-    it('will begin with playerX toggled as the active player', function() {
-        expect(testGame.playerX.active).toBeDefined(true);
-    });
+  it('will begin with playerX toggled as the active player', function() {
+    expect(testGame.playerX.active).toBeDefined(true);
+  });
 
-    it('will begin with playerO toggled as the inactive player', function() {
-        expect(testGame.playerO.active).toBeDefined(false);
-    });
+  it('will begin with playerO toggled as the inactive player', function() {
+    expect(testGame.playerO.active).toBeDefined(false);
+  });
 
   describe('switchTurn', function() {
     it('will switch Player X from the active player to the inactive player', function() {
-        expect(testGame.playerX.active).toEqual(true);
-        testGame.switchTurn();
-        expect(testGame.playerX.active).toEqual(false);
+      expect(testGame.playerX.active).toEqual(true);
+      testGame.switchTurn();
+      expect(testGame.playerX.active).toEqual(false);
     });
 
     it('will switch Player O from the inactive player to the active player', function() {
-        expect(testGame.playerO.active).toEqual(false);
-        testGame.switchTurn();
-        expect(testGame.playerO.active).toEqual(true);
+      expect(testGame.playerO.active).toEqual(false);
+      testGame.switchTurn();
+      expect(testGame.playerO.active).toEqual(true);
     });
+  });
+
+  describe('isDone', function() {
+    it('will return false if the game is not finished', function() {
+      expect(testGame.isDone()).toEqual(false);
+      testGame.boardArray = [["X", "O", "X"], ["O", "X", " "], [" ", "X", "O"]];
+      expect(testGame.isDone()).toEqual(false);
+    });
+
+    it('will return true when there\'s a winner', function() {
+      expect(testGame.isDone()).toEqual(false);
+      testGame.boardArray = [["X", "X", "X"], ["O", "X", "O"], [" ", "X", "O"]];
+      expect(testGame.isDone()).toEqual(true);
+    });
+  });
+
+  it('will return true when there\'s a tie', function() {
+    expect(testGame.isDone()).toEqual(false);
+    testGame.boardArray = [["X", "O", "X"], ["O", "X", "O"], ["X", "X", "O"]];
+    expect(testGame.isDone()).toEqual(true);
   });
 });
--- src/game.js
+++ src/game.js
@@ -5,6 +5,7 @@ var Game = function() {
   this.gameBoard = new Board();
   this.playerX = new Player('X');
   this.playerO = new Player('O');
+  var winner;
 };
 
 Game.prototype.switchTurn = function() {
@@ -12,9 +13,36 @@ Game.prototype.switchTurn = function() {
   this.playerO.active = !(this.playerO.active);
 };
 
-// Game.prototype.isDone = function() {
-//
-// };
+Game.prototype.isDone = function() {
+  var checkedMark;
+  var possibleWinner;
+
+  if (this.playerX.active === true) {
+    checkedMark = this.playerX.mark;
+    possibleWinner = this.playerX;
+  } else {
+    checkedMark = this.playerO.mark;
+    possibleWinner = this.playerO;
+  }
+
+  console.log(checkedMark);
+
+  if ((this.gameBoard.boardArray[0][0] == checkedMark && this.gameBoard.boardArray[1][0] == checkedMark && this.gameBoard.boardArray[2][0] == checkedMark) || (this.gameBoard.boardArray[0][0] == checkedMark && this.gameBoard.boardArray[1][1] == checkedMark && this.gameBoard.boardArray[2][2] == checkedMark) || (this.gameBoard.boardArray[0][0] == checkedMark && this.gameBoard.boardArray[0][1] == checkedMark && this.gameBoard.boardArray[0][2] == checkedMark) || (this.gameBoard.boardArray[0][1] == checkedMark && this.gameBoard.boardArray[1][1] == checkedMark && this.gameBoard.boardArray[2][1] == checkedMark) || (this.gameBoard.boardArray[0][2] == checkedMark && this.gameBoard.boardArray[1][2] == checkedMark && this.gameBoard.boardArray[2][2] == checkedMark) || (this.gameBoard.boardArray[0][2] == checkedMark && this.gameBoard.boardArray[1][1] == checkedMark && this.gameBoard.boardArray[2][0] == checkedMark) || (this.gameBoard.boardArray[1][0] == checkedMark && this.gameBoard.boardArray[1][1] == checkedMark && this.gameBoard.boardArray[1][2] == checkedMark) || (this.gameBoard.boardArray[2][0] == checkedMark && this.gameBoard.boardArray[2][1] == checkedMark && this.gameBoard.boardArray[2][2] == checkedMark)) {
+
+    console.log("winner checked");
+
+    winner = possibleWinner;
+    return true;
+
+  } else if (this.gameBoard.isFull() === true) {
+    winner = "Tie";
+    return true;
+
+  } else {
+    return false;
+  }
+
+};
 
 
 export default Game;
