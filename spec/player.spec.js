faea63bfe09d7df5dc3d32594df8a85a2fb69787
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-12-14 09:13:07 -0800

    Added 'before each' test functionality so that our variables are new objects for each 'it' test

--- spec/player.spec.js
+++ spec/player.spec.js
@@ -2,8 +2,14 @@ import Player from 'player';
 import Game from 'game';
 
 describe('Player', function() {
-    var testPlayerX = new Player('X');
-    var testPlayerO = new Player('O');
+    var testPlayerX;
+    var testPlayerO;
+    var testGame;
+    beforeEach(function() {
+        testPlayerX = new Player('X');
+        testPlayerO = new Player('O');
+        testGame = new Game();
+    });
 
     it('will create two players, each with a different mark', function() {
         expect(testPlayerX.mark).toEqual('X');
@@ -12,17 +18,13 @@ describe('Player', function() {
 
     describe('setMark', function() {
         it('will add applicable mark to the game\'s board', function() {
-            var testGame = new Game();
-
             expect(testPlayerX.setMark(testGame.gameBoard, 0, 0)).toEqual(testPlayerX.mark);
             expect(testPlayerO.setMark(testGame.gameBoard, 2, 2)).toEqual(testPlayerO.mark);
         });
 
         it('will not allow a player to set a mark in a space that\'s already been set', function() {
-            var testGame = new Game();
-
             testPlayerX.setMark(testGame.gameBoard, 0, 0);
-            
+
             expect(function () {
                 testPlayerO.setMark(testGame.gameBoard, 0, 0);
             }).toThrow();
