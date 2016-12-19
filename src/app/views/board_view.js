7a2cc624c985435520d215b7e54b4bd98b8377ed
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-12-19 12:56:04 -0800

    css styling

--- src/app/views/board_view.js
+++ src/app/views/board_view.js
@@ -9,9 +9,10 @@ var BoardView = Backbone.View.extend({
   },
 
   markSpot: function(spot) {
-      console.log(spot);
+    //   console.log(spot);
       var spotRow = spot.currentTarget.attributes.row.value;
       var spotCol = spot.currentTarget.attributes.col.value;
+    //   will comment these next 4 lines out in wave 3:
       console.log('spotRow is:');
       console.log(spotRow);
       console.log('spotCol is:');
