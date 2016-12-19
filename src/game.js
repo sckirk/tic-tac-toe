f58949f5d7115d26a22fddcbb97cb92f5205f687
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-12-19 15:12:54 -0800

    updated comments

--- src/game.js
+++ src/game.js
@@ -27,7 +27,7 @@ Game.prototype.isDone = function() {
     var checkedMark = this.currentPlayer.mark;
 
     if (
-        // the two diagonal winning possibilities:
+        // the two 'diagonal' winning possibilities:
         (this.gameBoard.boardArray[0][0] == checkedMark &&
         this.gameBoard.boardArray[1][1] == checkedMark &&
         this.gameBoard.boardArray[2][2] == checkedMark) ||
@@ -36,7 +36,7 @@ Game.prototype.isDone = function() {
         this.gameBoard.boardArray[1][1] == checkedMark &&
         this.gameBoard.boardArray[2][0] == checkedMark) ||
 
-        // the three down winning possibilities:
+        // the three 'down' winning possibilities:
         (this.gameBoard.boardArray[0][0] == checkedMark &&
         this.gameBoard.boardArray[1][0] == checkedMark &&
         this.gameBoard.boardArray[2][0] == checkedMark) ||
@@ -49,7 +49,7 @@ Game.prototype.isDone = function() {
         this.gameBoard.boardArray[1][2] == checkedMark &&
         this.gameBoard.boardArray[2][2] == checkedMark) ||
 
-        // the three across winning possibilities:
+        // the three 'across' winning possibilities:
         (this.gameBoard.boardArray[0][0] == checkedMark &&
         this.gameBoard.boardArray[0][1] == checkedMark &&
         this.gameBoard.boardArray[0][2] == checkedMark) ||
@@ -63,12 +63,12 @@ Game.prototype.isDone = function() {
         this.gameBoard.boardArray[2][2] == checkedMark)) {
 
         this.winner = this.currentPlayer;
-        this.sessionGameCount += 1;
+        // this.sessionGameCount += 1; might turn on later
         return true;
 
     } else if (this.gameBoard.isFull() === true) {
         this.winner = "Tie game, no winner this round!";
-        this.sessionGameCount += 1;
+        // this.sessionGameCount += 1; might turn on later
         return true;
 
     } else {
