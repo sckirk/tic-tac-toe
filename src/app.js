356dbb1b8a46681998bd07658017620436bd4ee2
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-12-21 13:53:05 -0800

    prepping to read data from the API via the collection

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import $ from 'jquery';
 
+import GameList from 'app/collections/game_list';
 import ApplicationView from 'app/views/application_view';
 
 $(document).ready(function() {
+    var gameList = new GameList();
+    gameList.fetch();
+
     var appView = new ApplicationView({
         el: $('#application'),
+        model: gameList
     });
 
     appView.render();
